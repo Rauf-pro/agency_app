5214d122c390abd0d847593ca423915af8942cc5
rrauf8178 <rrauf8178@gmail.com>
2024-04-04 19:22:24 +0400

    add: style for navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
+import Navbar from "./components/navbar/Navbar";
 import "./layout.scss";
 
 function App() {
   return <>
-    <div className="layout">Hello</div>
+    <div className="layout"><Navbar/></div>
   </>;
 }
 
---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import "../navbar.scss";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="Logo" />
+          <span>RafaAgency</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+      <a href="/">Sign in</a>
+        <a href="/">Sign up</a>
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
