0aa933162d5b89de2c4455321c9fa801a420a77f
rrauf8178 <rrauf8178@gmail.com>
2024-04-06 17:41:12 +0400

    add: filter component wit style

--- client/src/routes/listPage/listPage.jsx
+++ client/src/routes/listPage/listPage.jsx
@@ -1,19 +1,23 @@
+import Card from "../../components/card/Card";
 import Filter from "../../components/filter/Filter";
 import { listData } from "../../lib/dummydata";
 import "./listPage.scss";
 
 function ListPage() {
-    const data = listData;
-    return (
-        <div className="listPage">
-            <div className="listContainer">
-                <div className="wrapper">
-                    <Filter/>
-                </div>
-            </div>
-            <div className="mapContainer">map</div>
+  const data = listData;
+  return (
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map((item) => (
+            <Card key={item.id} />
+          ))}
         </div>
-    );
+      </div>
+      <div className="mapContainer">map</div>
+    </div>
+  );
 }
 
 export default ListPage;
