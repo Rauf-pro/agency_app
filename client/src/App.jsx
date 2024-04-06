f5eb4cf3bd4282eb087488248b29af526d215430
rrauf8178 <rrauf8178@gmail.com>
2024-04-06 21:31:35 +0400

    add: add single page and finish ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import ListPage from "./routes/listPage/listPage";
 import HomePage from "./routes/homePage/homePage";
 import Layout from "./routes/layout/layout";
 import LoginPage from "./routes/login/loginPage";
+import SinglePage from "./routes/singlePage/singlePage";
 
 function App() {
   const router = createBrowserRouter([
@@ -13,7 +14,10 @@ function App() {
         { path: "/", element: <HomePage /> },
         { path: "/list", element: <ListPage /> },
         { path: "/login", element: <LoginPage /> },
-
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
       ],
     },
   ]);
