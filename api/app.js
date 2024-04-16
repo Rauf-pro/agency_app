c64fa11a3d74f483ad64cec36ea2446fd4be5b9d
rrauf8178 <rrauf8178@gmail.com>
2024-04-16 20:11:24 +0400

    add: login register authentication authorize and not authenticated functions

--- api/app.js
+++ api/app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import postRoute from "./routes/post.route.js";
+// import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
+import testRoute from "./routes/test.route.js";
 
 const app = express();
 
@@ -10,8 +11,10 @@ app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/posts", postRoute);
+// app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/test", testRoute);
+
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
---
+++ api/controllers/test.controller.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+
+export const shouldBeLoggedIn = (req, res) => {
+  const token = req.cookies.token;
+
+  if (!token) return res.status(401).json({ message: "Not Authenticated!" });
+  
+
+  jwt.verify(token, process.env.JWT_SECRET, async (err, payload) => {
+    if (err) {
+      return res.status(403).json({ message: "Token is not Valid!" });
+    }
+  });
+
+  res.status(200).json({ message: "You are Authenticated!" });
+};
+
+export const shouldBeAdmin = (req, res) => {
+  const token = req.cookies.token;
+
+  if (!token) return res.status(401).json({ message: "Not Authenticated!" });
+  
+
+  jwt.verify(token, process.env.JWT_SECRET, async (err, payload) => {
+    if (err) return res.status(403).json({ message: "Token is not Valid!" });
+
+    if (!payload.isAdmin) return res.status(403).json({ message: "Not authorized!" });
+    
+  });
+
+  res.status(200).json({ message: "You are Authenticated!" });
+};
+
